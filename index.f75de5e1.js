8aded38c3f57630708897e51dad21c6e84fe3e97
Bohdan Klymeniuk <klymeniuk.bohdan@gmail.com>
2024-07-07 15:46:11 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const largeImg = document.getElementById("largeImg");
+const galleryList = document.querySelector(".gallery__list");
+galleryList.addEventListener("click", (events)=>{
+    events.preventDefault();
+    const link = events.target.closest(".list-item__link");
+    largeImg.src = link.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
